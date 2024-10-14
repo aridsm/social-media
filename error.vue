157e80d5edcb1b2facf6beea5ac63a40cd2fffd2
ariane <aniemorelanie@gmail.com>
2024-10-13 22:58:13 -0300

    feat: add página de erro 404

---
+++ error.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const router = useRouter();
+const error = useError();
+</script>
+
+<template>
+  <div
+    class="flex h-[100vhmin] min-h-[100vhmin] md:h-screen md:min-h-screen max-w-[2000px] w-full mx-auto"
+  >
+    <AppMenu class="hidden xl:flex" />
+    <div class="pt-24 flex flex-col items-center w-full flex-1">
+      <icon icon="fa-solid fa-circle-exclamation" class="text-[2rem] mb-4" />
+      <span class="text-lg font-bold">Ops!</span>
+      <span class="text-lg font-bold">Error {{ error.statusCode }}</span>
+      <span>{{ error.message }}</span>
+      <AppBtn @click="router.push('/')" class="mt-4">Go to home</AppBtn>
+    </div>
+  </div>
+</template>
